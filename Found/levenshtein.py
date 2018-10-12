5592b1f95f94b753d9cad927d87a01bf3ba7bb0f
Denis Belavin <belavin.d.andreevich@gmail.com>
2018-10-12 15:29:26 +0300

    HotFix (#001)

--- Found/levenshtein.py
+++ Found/levenshtein.py
@@ -2,11 +2,11 @@
 
 
 def lev(s1, s2):
-    if s1:
+    if not s1:
         return len(s2)
-    elif s2:
+    elif not s2:
         return len(s1)
-    mtx = [[0] * (len(s2) + 1) for _ in range(len(s1 + 1))]
+    mtx = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
     for i in range(1, len(s1) + 1):
         mtx[i][0] = i
     for j in range(1, len(s2) + 1):
